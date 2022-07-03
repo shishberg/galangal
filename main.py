570752a841eb3e1d3a13552845570da263dc2c5f
David McLeish <dave@mcleish.id.au>
2022-07-03 16:43:11 +1000

    Move player information to Player class.

--- main.py
+++ main.py
@@ -10,39 +10,51 @@ BLUE = (63, 127, 255)
 
 FPS = 60
 
-PLAYER_IMAGE = pygame.image.load("Assets/plate.png")
-PLAYER_WIDTH = PLAYER_IMAGE.get_width()
-PLAYER_X = 450
-PLAYER_LEFT = False
-PLAYER_RIGHT = False
-PLAYER_SPEED = 5
+KEY_LEFT = False
+KEY_RIGHT = False
+
+class Player(object):
+    def __init__(self):
+        self.image = pygame.image.load("Assets/plate.png")
+        self.width = self.image.get_width()
+        self.x = 450
+        self.y = 400
+        self.speed = 5
+
+    def update(self):
+        global KEY_LEFT, KEY_RIGHT
+        if KEY_LEFT:
+            self.x -= self.speed
+        if KEY_RIGHT:
+            self.x += self.speed
+        self.x = max(0, min(900 - self.width, self.x))
+
+    def draw(self, win):
+        win.blit(self.image, (self.x, self.y))
+
+PLAYER = Player()
 
 def draw_window():
     WIN.fill(BLUE)
-    WIN.blit(PLAYER_IMAGE, (PLAYER_X, 400))
+    PLAYER.draw(WIN)
     pygame.display.update()
 
 def handle_key_down(key):
-    global PLAYER_LEFT, PLAYER_RIGHT
+    global KEY_LEFT, KEY_RIGHT
     if key == pygame.K_LEFT:
-        PLAYER_LEFT = True
+        KEY_LEFT = True
     elif key == pygame.K_RIGHT:
-        PLAYER_RIGHT = True
+        KEY_RIGHT = True
 
 def handle_key_up(key):
-    global PLAYER_LEFT, PLAYER_RIGHT
+    global KEY_LEFT, KEY_RIGHT
     if key == pygame.K_LEFT:
-        PLAYER_LEFT = False
+        KEY_LEFT = False
     elif key == pygame.K_RIGHT:
-        PLAYER_RIGHT = False
+        KEY_RIGHT = False
 
 def physics():
-    global PLAYER_X, PLAYER_LEFT, PLAYER_RIGHT
-    if PLAYER_LEFT:
-        PLAYER_X -= PLAYER_SPEED
-    if PLAYER_RIGHT:
-        PLAYER_X += PLAYER_SPEED
-    PLAYER_X = max(0, min(900 - PLAYER_WIDTH, PLAYER_X))
+    PLAYER.update()
 
 def main():
     clock = pygame.time.Clock()
